b045c9be48cad80f3dca50b670ed3bfb47c0368f
Textspot <sachinkatarki1995@gmail.com>
2021-12-28 15:50:43 +0530

    Added:Welcome to the Factors Number in Programme

--- src/com/bridgelabz/basiccoreprogramme/EvenorOdd.java
+++ src/com/bridgelabz/basiccoreprogramme/EvenorOdd.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class EvenorOdd {
 
 	public static void main(String[] args) {
-		 int num;
-		 System.out.println("Enter the value ");
-		 Scanner sc = new Scanner(System.in);
-		 num=sc.nextInt();
-		 sc.close();
-		 if (num%2==0) {
-			 System.out.println("Entered number is Even");
-		 }
-		 else {
-			 System.out.println("Entered number is Odd");
-		 }
+		int num;
+		System.out.println("Enter the value ");
+		Scanner sc = new Scanner(System.in);
+		num = sc.nextInt();
+		sc.close();
+		if (num % 2 == 0) {
+			System.out.println("Entered number is Even");
+		} else {
+			System.out.println("Entered number is Odd");
+		}
 
-	   }
+	}
 
-     }
+}
