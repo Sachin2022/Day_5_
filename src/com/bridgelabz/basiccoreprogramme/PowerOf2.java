93078cbf43a6df6e1e4efb8d6f5fbb4e0460bdd7
Textspot <sachinkatarki1995@gmail.com>
2021-12-28 16:19:59 +0530

    Added:To Calculate the Power of Two Number in Programme

---
+++ src/com/bridgelabz/basiccoreprogramme/PowerOf2.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.basiccoreprogramme;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the power of 2 : ");
+		Scanner sc = new Scanner(System.in);
+		int num = 2;
+		int power;
+		power = sc.nextInt();
+		sc.close();
+
+		System.out.println(Math.pow(num, power));
+
+	  }
+
+	}
+
+
