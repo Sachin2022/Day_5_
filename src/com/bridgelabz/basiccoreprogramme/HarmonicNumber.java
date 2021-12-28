479473e794acee491da3b1398cbe5efa9d590d68
Textspot <sachinkatarki1995@gmail.com>
2021-12-28 16:01:10 +0530

    Added:Welcome to the Harmonic Numbers Programme

---
+++ src/com/bridgelabz/basiccoreprogramme/HarmonicNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.basiccoreprogramme;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+
+	public static void main(String[] args) {
+		double harmonic = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number to find harmonic value:");
+		int num = scanner.nextInt();
+		scanner.close();
+
+		for (int i = 1; i <= num; i++) {
+			harmonic += (double) 1 / i;
+			System.out.println(" Harmonic " + i + ": " + harmonic);
+		}
+
+	  }
+
+    }
